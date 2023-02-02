754f376b82e0d9d4e3e69a20ce2c7728b9a1120c
Alexandra Cojocari <116676337+aleuvt0202@users.noreply.github.com>
2023-02-02 19:36:32 +0200

    Add files via upload
    all files

---
+++ start.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="design.css"> 
+    <link rel="stylesheet" href="card.css"> 
+    <title>CV</title>
+</head>
+<body class="center">
+
+<div class="box">
+    <header>
+        <h1 style="font-size: 50px;">Cojocari Alexandra</h1>
+    </header>
+
+
+    <section>
+        <nav>
+            <i class='bx bx-mail-send'></i><a>cojocarialexandra@icloud.com</a>
+            <i class='bx bxs-phone' ></i><a>+40 0749873562</a>
+            <i class='bx bxs-home-smile' ></i><a>Adress: Apateu nr11 streat, Timisoara</a>
+        </nav>
+        <nav>
+            <i class='bx bx-user-pin' ></i><a>Birth date: 13 sept 2002</a>
+        </nav>
+        <article>
+            <div class="img">
+            </div>
+        </article>
+        <aside>
+        <h1> Carrier Objective</h1>
+        <p>As a computer science student, I find that I enjoy spending time working on projects on different topics.
+        Combining my knowledge from school and what I study privately, my aspirations are to further develop
+        my skills in this field. I am excited about this journey and hope to learn more from people who have
+        already been through this path.</p>
+        </aside>
+    </section>
+</div>
+
+
+<i class='bx bx-book-open' style="font-size: 50px;"></i>
+<h1>Education</h1>
+
+<div class="container">
+    <div class="stanga">
+        <p><strong>Faculty of Mathematics and Informatics</strong> | West Univesity Timisoara</p>
+        <p>Informatics</p>
+        <p><strong>Pedagogic Carmen Sylva High School</strong> | Timisoara</p>
+        <p>Natural Science</p>
+    </div>
+    <div class="dreapta">
+        <p style="padding-bottom: 10%;">October 2021 - present</p>
+        <p>September 2017 - June 2021</p>
+    </div>
+</div>
+
+
+
+<div class="container1">
+    <i class='bx bx-briefcase' style="font-size: 50px;"></i>
+    <h1>Work Experience</h1>
+
+    <div style="padding-left: 10%; padding-right: 10%;">
+        <p><strong>Supply Chain Management Intership</strong> | Continental</p>
+        <p>June 2022 - present</p>
+        <p>Within this job I've learn to work with Microsoft Excel and SAP. My tasks always had a deadline and needed to be respected.</p>
+        <p><strong>Skills learned:</strong></p>
+        <div style="text-align: center; font-size: 20px;">
+            <dl>
+                <dt>Microsoft Excel, Outlook, SAP</dt>
+                <dt>Attention to details</dt>
+                <dt>Respecting a schedule</dt>
+                <dt>Office work</dt>
+            </dl>
+        </div>
+    </div>
+</div>
+
+<i class='bx bx-code-alt' style="font-size: 50px; padding-top: 5%;"></i>
+<h1>Projects</h1>
+
+<section class="hero-section">
+  <div class="card-grid">
+    <a class="card">
+      <div class="card__background" style="background-image: url(nokia.jpg)"></div>
+       <p class="img__description" style="color: white;">Graphical Interfaces created with PowerBI
+       </p>
+        <p class="img__description" style="color: white;padding-top: 20%;">Language used: Microsoft Power BI, SQL, HTML, CSS, JS</p>
+        <p class="img__description" style="color: white;padding-top: 50%;">Autumn 2022</p>
+
+
+    </a>
+    <a class="card">
+      <div class="card__background" style="background-image: url(react.jpg)"></div>
+        <p class="img__description" style="color: white;">GDSC React Native Training on mobile apss</p>
+        <p class="img__description" style="color: white;padding-top: 20%;">Language used: React Native
+       </p>
+       <p class="img__description" style="color: white;padding-top: 40%;">Autumn 2022</p>
+
+    </a>
+    <a class="card">
+      <div class="card__background" style="background-image: url(feaa.jpg)"></div>
+        <p class="img__description" style="color: white;">"Creative Timisoara"</p>
+        <p class="img__description" style="color: white;padding-top: 20%;">Team contest
+       </p>
+       <p class="img__description" style="color: white;padding-top: 40%;">Spring 2018</p>
+    </a>
+
+    <a class="card">
+      <div class="card__background" style="background-image: url(web.jog.jpg)"></div>
+        <p class="img__description" style="color: white;">Web project</p>
+        <p class="img__description" style="color: white;padding-top: 20%;">Language used: HTML, CSS, JS</p>
+       <p class="img__description" style="color: white;padding-top: 40%;">Spring 2022,Winter 2023</p>
+    </a>
+    </li>
+  <div>
+</section>
+
+<i class='bx bxs-bar-chart-alt-2' style="font-size: 50px; padding-top: 5%;"></i>
+<h1>Skills</h1>
+
+<div class="container" style="font-size: 23px; padding-bottom:30%">
+    <div class="stanga" style="padding-left: 20%;">
+        <h1>Programming</h1>
+        <dl>
+            <dt>Office Package</dt>
+            <dt>Excel, Power Bi</dt>
+            <dt>C/C++</dt>
+            <dt>Java</dt>
+            <dt>HTML, CSS, JS</dt>
+            <dt>Databases, SQL</dt>
+        </dl>
+    </div>
+    <div class="dreapta" style="padding-right: 20%;">
+        <h1>Other</h1>
+        <dl>
+            <dt>Analytical thinking</dt>
+            <dt>Attention to detalies</dt>
+            <dt>Team player</dt>
+            <dt>Good communication</dt>
+        </dl>
+    </div>
+</div>
+
+
+
+</body>
+</html>
